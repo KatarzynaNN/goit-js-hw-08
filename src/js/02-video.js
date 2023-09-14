49b87b22e6a852ed234b743e7c30bae64f09a9a4
KatarzynaNN <kat.nedza@gmail.com>
2023-09-14 18:57:11 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timeKey = 'videoplayer-current-time';
+
+const onTimeUpdate = function (data) {
+  localStorage.setItem(timeKey, data.seconds);
+};
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+const timeSaved = localStorage.getItem(timeKey);
+player.setCurrentTime(timeSaved);
