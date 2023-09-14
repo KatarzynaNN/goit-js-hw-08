823ddacc0b1e0ffc43b243610f13873c231ea8e0
KatarzynaNN <kat.nedza@gmail.com>
2023-09-14 19:53:37 +0200

    task3 part 1
    input

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const inputKey = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onInput), 500);
+
+function onInput(event) {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  const formValue = {
+    email: email,
+    message: message,
+  };
+  localStorage.setItem(inputKey, JSON.stringify(formValue));
+}
