e26aea2a77262c1ea0eda6ac0342ef0a0d48b516
KatarzynaNN <kat.nedza@gmail.com>
2023-09-14 21:00:35 +0200

    task3 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,14 +4,35 @@ const form = document.querySelector('.feedback-form');
 
 const inputKey = 'feedback-form-state';
 
-form.addEventListener('input', throttle(onInput), 500);
+let formValue = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener('input', throttle(onInput, 500));
 
 function onInput(event) {
   const email = form.elements.email.value;
   const message = form.elements.message.value;
-  const formValue = {
+  formValue = {
     email: email,
     message: message,
   };
   localStorage.setItem(inputKey, JSON.stringify(formValue));
+}
+
+const formValueJSON = localStorage.getItem(inputKey);
+if (formValueJSON) {
+  formValue = JSON.parse(formValueJSON);
+  form.elements.email.value = formValue.email;
+  form.elements.message.value = formValue.message;
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(formValue);
+  localStorage.removeItem(inputKey);
+  form.reset();
 }
